daf88db68d1d13e45adc2004e068158cb03299dd
Kiril Hansen <kiril@snabelkat.dk>
2025-04-28 14:37:17 +0200

    Initial Commit

---
+++ src/main/java/net/tetto/moreenderpearls/MoreenderpearlsClient.java
@@ -0,0 +1,10 @@
+package net.tetto.moreenderpearls;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreenderpearlsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/tetto/moreenderpearls/MoreenderpearlsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tetto.moreenderpearls;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreenderpearlsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
